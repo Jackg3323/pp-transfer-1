7c65fcf96572551f3ae754fd2a8b9a5552794939
Manav Misra <manavm@visionwebsoft.com>
2022-08-22 11:53:58 -0500

    Get App test working using elements
    Don't use ids with RTL

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,26 @@
-import Form from "components/Form/Form";
 import { useState } from "react";
 import "./App.css";
+import Form from "./components/Form/Form";
 
 const balance = 100;
 
 function App() {
   const [amt, setAmt] = useState(0);
 
-  const handleFocus = (event) => {
-    console.log("focused");
-  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-  const handleBlur = (event) => {
-    console.log("blurred");
+    setAmt(event.target.elements[0].value);
+
+    event.target.reset();
   };
 
   return (
     <>
-      {/* TODO: Add another handler to pass in for 'onFocus'.
-        See 'onBlur' to also conditionally render.
-        This will clear off the last transfer message (e.g. conditional rendering)
-      */}
-      <Form setAmt={setAmt} handleFocus={handleFocus} />
+      <Form handleSubmit={handleSubmit} />
 
-      {amt > 0 &&
-        (amt > balance ? (
-          <p>No! U don&apos;t have money</p>
-        ) : (
-          <p>Ok. You transferred {amt}.</p>
-        ))}
-
-      {/* TODOs:
-            1. Don't update the info below if I am going negative.
-            2.
-       */}
-      {/* TODO: Conditionally render this as long as the balance is not negative */}
-      <p>You have {balance - amt} left</p>
+      <p>You transferred {amt}</p>
+      <p>Your balance is now: {balance - amt}</p>
     </>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("renders learn react link", () => {
+it("renders the updated amount when the form is submitted", () => {
+  const transferAmt = 50;
+  const expected = `You transferred ${transferAmt}`;
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const input = screen.getByLabelText("Transfer Amount");
+  userEvent.type(input, transferAmt.toString());
+  userEvent.click(screen.getByRole("button"));
+
+  expect(screen.getByText(expected)).toBeInTheDocument();
 });
