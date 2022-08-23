25eac5cda04259b89cc1577e68e65c223e79f1a3
Manav Misra <manavm@visionwebsoft.com>
2022-08-23 08:49:28 -0500

    test: use hard-coded balance in each test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,13 +2,8 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-let balance;
-
-beforeEach(() => {
-  balance = 100;
-});
-
 it("renders the updated amount when the form is submitted", () => {
+  const balance = 100;
   const transferAmt = 50;
   const expectedTransferMsg = `You transferred ${transferAmt}`;
   const expectedBalanceMsg = `Your balance is now: ${balance - transferAmt}`;
@@ -23,6 +18,7 @@ it("renders the updated amount when the form is submitted", () => {
 });
 
 it("prevents negative balances from occurring", () => {
+  const balance = 100;
   const transferAmt = 1000;
   const expectedMsg = `You can't transfer more than ${balance}`;
 
