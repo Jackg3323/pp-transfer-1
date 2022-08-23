2d99328a1b17656e3192ca0b544834c47d792bbf
Manav Misra <manavm@visionwebsoft.com>
2022-08-23 08:52:47 -0500

    test: move the test balance to top of file
    Renamed it to testBalance for clarity.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,11 +2,14 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
+const testBalance = 100;
+
 it("renders the updated amount when the form is submitted", () => {
-  const balance = 100;
   const transferAmt = 50;
   const expectedTransferMsg = `You transferred ${transferAmt}`;
-  const expectedBalanceMsg = `Your balance is now: ${balance - transferAmt}`;
+  const expectedBalanceMsg = `Your balance is now: ${
+    testBalance - transferAmt
+  }`;
 
   render(<App />);
   const input = screen.getByLabelText("Transfer Amount");
@@ -18,9 +21,8 @@ it("renders the updated amount when the form is submitted", () => {
 });
 
 it("prevents negative balances from occurring", () => {
-  const balance = 100;
   const transferAmt = 1000;
-  const expectedMsg = `You can't transfer more than ${balance}`;
+  const expectedMsg = `You can't transfer more than ${testBalance}`;
 
   render(<App />);
   const input = screen.getByLabelText("Transfer Amount");
