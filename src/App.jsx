e5d72c075f0ab7c893c554e92b8476ae5267b56a
Manav Misra <manavm@visionwebsoft.com>
2022-08-23 08:47:06 -0500

    refactor: put balance in the state
    This is probably overly complex. Will explore refactoring.

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,31 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
 
-let balance = 100;
-
 function App() {
-  const [transferStatus, setTransferStatus] = useState({ amt: 0, msg: "" });
+  const [transferStatus, setTransferStatus] = useState({
+    amt: 0,
+    msg: "",
+    balance: 100,
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const amt = event.target.elements[0].value;
+    const transferAmt = event.target.elements[0].value;
 
-    if (amt > balance) {
-      setTransferStatus({
-        amt: 0,
-        msg: `You can't transfer more than ${balance}`,
-      });
+    if (transferAmt > transferStatus.balance) {
+      setTransferStatus((prevTransferStatus) => ({
+        ...prevTransferStatus,
+        msg: `You can't transfer more than ${transferStatus.balance}`,
+      }));
     } else {
-      balance -= amt;
+      transferStatus.balance -= transferAmt;
 
-      setTransferStatus({
-        amt,
+      setTransferStatus((prevTransferStatus) => ({
+        ...prevTransferStatus,
+        amt: transferAmt,
         msg: "You transferred ",
-      });
+      }));
     }
 
     event.target.reset();
@@ -37,7 +40,7 @@ function App() {
         {transferStatus.msg} {transferStatus.amt > 0 && transferStatus.amt}
       </p>
 
-      <p>Your balance is now: {balance}</p>
+      <p>Your balance is now: {transferStatus.balance}</p>
     </>
   );
 }
